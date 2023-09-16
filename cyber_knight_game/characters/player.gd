365f1fba869b6c5c270448eccb79197a0e05c82e
Alfred Zhu <alfredzhu@utexas.edu>
2023-09-16 00:08:32 -0500

    Added comments and slightly modified dash implementation

--- cyber_knight_game/characters/player.gd
+++ cyber_knight_game/characters/player.gd
@@ -1,29 +1,29 @@
 extends CharacterBody2D
 
-
-const SPEED = 350.0
-const JUMP_VELOCITY = -650.0
+const SPEED = 350.0 # X movement speed
+const JUMP_VELOCITY = -650.0# Y movement speed. In Godot going up is negative
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-const DASH_VELOCITY = 3000.0 
+const DASH_VELOCITY = 3400.0 
 var can_dash = true
 var is_dashing = false
-var dash_direction = Vector2.ZERO
+var dash_direction = 0
 var facing
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 
 func dash():
 	
 	if Input.is_action_pressed("left"):
 		facing = 'l'
-		dash_direction = Vector2(-1, 0)
+		dash_direction = -1
 		
 	if Input.is_action_pressed("right"):
 		facing = 'r'
-		dash_direction = Vector2(1, 0)
+		dash_direction = 1
 	
 	if Input.is_action_just_pressed("dash") and can_dash:
+		
+		# Animations
 		if facing == 'r':
 			$AnimatedSprite2D.flip_h = false
 			$AnimatedSprite2D.animation = "lwalk"
@@ -31,15 +31,20 @@ func dash():
 		if facing == 'l':
 			$AnimatedSprite2D.flip_h = true
 			$AnimatedSprite2D.animation = "lwalk"
-		velocity = dash_direction.normalized() * DASH_VELOCITY
 		
-		velocity.x = lerp(velocity.x,0.0,0.25) 
+		velocity.x = dash_direction * DASH_VELOCITY
+		
 		
 		can_dash = false
+		# is_dashing variable is mainly used so that regular left and right movement
+		# will not interrupt dashing movement
 		is_dashing = true
+		# This quick timer will allow the is_dashing variable to do its job at
+		# preventing left and right movements from interrupting dash
 		await get_tree().create_timer(0.1).timeout
 		is_dashing = false
-		await get_tree().create_timer(0.9).timeout
+		# regular dash cooldown. Effectively 1.1 seconds
+		await get_tree().create_timer(1.0).timeout
 		can_dash = true
 		
 
@@ -58,12 +63,12 @@ func _physics_process(delta):
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	
+	# not is_dashing is important
+	# allows the player to dash even when they are holding left or right
 	if direction and not is_dashing:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	
-	
+	# built in function that seems important lol
 	move_and_slide()
