441e27a336fce8c251658faae2ee7b80725084e8
kayleehk <kang.kaylee@utexas.edu>
2023-10-15 15:10:30 -0500

    Game over scene

--- cyber_knight_game/ui_stuff/game_over.gd
+++ cyber_knight_game/ui_stuff/game_over.gd
@@ -9,3 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_try_again_pressed():
+	get_tree().change_scene_to_file("res://levels/testing_stage.tscn")
+
+
+func _on_quit_pressed():
+		get_tree().change_scene_to_file("res://HUD/HUD.tscn")
