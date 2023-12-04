c7d9ad6f90f551aef59d085ca685910f684326d4
Alfred Zhu <87275944+Sketchfellow@users.noreply.github.com>
2023-12-03 23:32:44 -0600

    Added sfx and music to main menu and opening scene

--- cyber_knight_game/openingScene.gd
+++ cyber_knight_game/openingScene.gd
@@ -1,6 +1,8 @@
 extends Node2D
 var scene = 1
 
+var virusHappened = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.animation = '1' # Replace with function body.
@@ -13,6 +15,10 @@ func _process(delta):
 		scene += 1
 	if scene < 5:
 		$AnimatedSprite2D.animation = str(scene)
+	if scene == 3 and not virusHappened:
+		$AudioStreamPlayer2D2.play()
+		$AudioStreamPlayer2D.stop()
+		virusHappened = true
 	
 	if scene == 5:
 		get_tree().change_scene_to_file("res://HUD/HUD.tscn")
